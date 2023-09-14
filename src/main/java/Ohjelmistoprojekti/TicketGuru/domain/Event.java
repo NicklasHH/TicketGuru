d6db372c2723595eb311f10925d9b859d79c6038
Roope <roopejuuz@gmail.com>
2023-09-14 14:04:24 +0300

    Event class updated

--- src/main/java/Ohjelmistoprojekti/TicketGuru/domain/Event.java
+++ src/main/java/Ohjelmistoprojekti/TicketGuru/domain/Event.java
@@ -1,16 +1,79 @@
 package Ohjelmistoprojekti.TicketGuru.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Event {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long eventId;
-	
 	private String eventName;
-
 	private String date;
-
 	private int ticketCount;
-
 	private int venueId;
-
 	private String description;
+
+	public Event() {
+
+	}
+
+	public Event(String eventName, String date, int ticketCount, int venueId, String description) {
+		super();
+		this.eventName = eventName;
+		this.date = date;
+		this.ticketCount = ticketCount;
+		this.venueId = venueId;
+		this.description = description;
+	}
+
+	public long getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(long eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getTicketCount() {
+		return ticketCount;
+	}
+
+	public void setTicketCount(int ticketCount) {
+		this.ticketCount = ticketCount;
+	}
+
+	public int getVenueId() {
+		return venueId;
+	}
+
+	public void setVenueId(int venueId) {
+		this.venueId = venueId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
