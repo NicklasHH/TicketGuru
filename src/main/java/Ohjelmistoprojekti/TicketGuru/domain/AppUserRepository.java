c56871dc05d87628006479c1ec15ad90fb29e364
Nadja Liljeström <nliljestrom@gmail.com>
2023-09-21 13:52:21 +0300

    updated EventRestController

--- src/main/java/Ohjelmistoprojekti/TicketGuru/domain/AppUserRepository.java
+++ src/main/java/Ohjelmistoprojekti/TicketGuru/domain/AppUserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
 
-   // AppUser findByUsername(String username);
+   // AppUser findByUsername(String username); 
 }
