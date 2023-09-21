8c6b63522c37861af7265b83dfe3943134bf8f07
laguagu <49702497+laguagu@users.noreply.github.com>
2023-09-21 13:57:20 +0300

    added commanlineRunner bean

--- src/main/java/Ohjelmistoprojekti/TicketGuru/TicketGuruApplication.java
+++ src/main/java/Ohjelmistoprojekti/TicketGuru/TicketGuruApplication.java
@@ -1,15 +1,35 @@
 package Ohjelmistoprojekti.TicketGuru;
 
+import Ohjelmistoprojekti.TicketGuru.domain.Event;
+import Ohjelmistoprojekti.TicketGuru.domain.EventRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketGuruApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketGuruApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketGuruApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner demo(EventRepository eventRepository) {
+        return (args) -> {
+            // Luodaan Event entiteetti jos tietokanta on tyhjä
+            if (eventRepository.count() == 0) {
+                Event event = new Event();
+                event.setEventName("Esimerkkitapahtuma");
+                event.setDescription("Esim");
+                event.setEventDate("12-12-2023");
+                event.setEventTime("18:00");
+                event.setTicketCount(100);
+
+                eventRepository.save(event);
+            }
+        };
+    }
 }
 
 
