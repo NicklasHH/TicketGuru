181226a6cc801639c6989409997a004ec274fe3c
Nicklas <nicklas.akerman@myy.haaga-helia.fi>
2023-09-26 17:49:30 +0300

    Tehty roleRestController

--- src/main/java/Ohjelmistoprojekti/TicketGuru/Role/RoleRestController.java
+++ src/main/java/Ohjelmistoprojekti/TicketGuru/Role/RoleRestController.java
@@ -1,16 +1,23 @@
 package Ohjelmistoprojekti.TicketGuru.Role;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-import Ohjelmistoprojekti.TicketGuru.Event.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import Ohjelmistoprojekti.TicketGuru.AppUser.AppUser;
+import Ohjelmistoprojekti.TicketGuru.AppUser.AppUserRepository;
 
 @RestController
 @RequestMapping("/api/roles")
@@ -23,7 +30,10 @@ public class RoleRestController {
 		this.roleRepository = roleRepository;
 	}
 
-	@GetMapping("/") // http://localhost:8080/api/roles
+	@Autowired
+	private AppUserRepository appUserRepository;
+
+	@GetMapping // http://localhost:8080/api/roles
 	ResponseEntity<List<Role>> all() {
 		List<Role> roles = roleRepository.findAll(); // Hae kaikki roolit tietokannasta
 		if (!roles.isEmpty()) {
@@ -32,23 +42,65 @@ public class RoleRestController {
 			return ResponseEntity.notFound().build();// HTTP 404 Not Found
 		}
 	}
+
+	// Palauttaa roolin id perusteella
 	@GetMapping("/{id}")
 	public ResponseEntity<Role> getRole(@PathVariable Long id) {
 		Optional<Role> role = roleRepository.findById(id);
 		if (role.isPresent()) {
-			return ResponseEntity.ok(role.get());
+			return ResponseEntity.ok(role.get());// HTTP 200 OK
 		} else {
-			return ResponseEntity.notFound().build();
+			return ResponseEntity.notFound().build();// HTTP 404 Not Found
 		}
 	}
 
-	@DeleteMapping("/{id}") // http://localhost:8080/api/roles/1
-	public ResponseEntity<?> deleteRole(@PathVariable Long id) { // Hae rooli tietokannasta ja palauta vastaus
-		Optional<Role> roleOptional = roleRepository.findById(id);// Palauttaa roolin Id:N perusteella
+	// Palauttaa roolin id:n perusteella roolin nimen
+	@GetMapping("/{id}/rolename") // http://localhost:8080/api/roles/1/rolename
+	public ResponseEntity<Object> getRole(@PathVariable long id) {
+		Optional<Role> roleOptional = roleRepository.findById(id);
 		if (roleOptional.isPresent()) {
 			Role role = roleOptional.get();
-			roleRepository.deleteById(id); // Poistaa roolin Id:n perusteella
-			return ResponseEntity.ok(role); // HTTP 200 OK, palauttaa poistetun roolin tiedot
+			Map<String, String> jsonResponse = new HashMap<>(); // Luo Map-olio JSON-muotoa varten
+			jsonResponse.put("rolename", role.getRoleName());
+			return ResponseEntity.ok(jsonResponse); // HTTP 200 OK
+		} else {
+			return ResponseEntity.notFound().build(); // HTTP 404 Not Found
+		}
+	}
+
+	// lisätään uusi rooli
+	@PostMapping // http://localhost:8080/api/appusers
+	Role newRole(@RequestBody Role newRole) {
+
+		System.out.println("Adding new role: " + newRole);
+
+		return roleRepository.save(newRole);
+	}
+	
+	// muokataan olemassa olevaa roolia
+	@PutMapping("/{id}") // http://localhost:8080/api/roles/id
+	Role editRole(@RequestBody Role editedRole, @PathVariable Long id) {
+
+		editedRole.setRoleId(id);
+		System.out.println("Editing role: " + editedRole);
+		return roleRepository.save(editedRole);
+	}
+
+	@DeleteMapping("/{id}") // http://localhost:8080/api/roles/1
+	public ResponseEntity<?> deleteRole(@PathVariable Long id) { // Hae appuser tietokannasta ja palauta vastaus
+		Optional<Role> roleOptional = roleRepository.findById(id);// Palauttaa appuserin Id:N perusteella
+		if (roleOptional.isPresent()) {
+			Role role = roleOptional.get();
+
+			// Hae ja päivitä kaikki liittyvät roolit
+			List<AppUser> appUsers = appUserRepository.findByRole_RoleId(id);
+			for (AppUser appUser : appUsers) {
+				appUser.setRole(null);
+			}
+			appUserRepository.saveAll(appUsers);
+
+			roleRepository.deleteById(id); // Poistaa appuserin Id:n perusteella
+			return ResponseEntity.ok(role); // HTTP 200 OK, palauttaa poistetun appuserin tiedot
 		} else {
 			return ResponseEntity.notFound().build(); // HTTP 404 Not Found
 		}
