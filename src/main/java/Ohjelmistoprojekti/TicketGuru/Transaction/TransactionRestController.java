e907f7d5b138fe236fe6718c8c51c305c79c5010
Nadja Liljeström <nliljestrom@gmail.com>
2023-09-26 17:56:34 +0300

    TransactionRestController.java, added POST and GET by id

--- src/main/java/Ohjelmistoprojekti/TicketGuru/Transaction/TransactionRestController.java
+++ src/main/java/Ohjelmistoprojekti/TicketGuru/Transaction/TransactionRestController.java
@@ -8,8 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
+
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -22,6 +28,7 @@ public class TransactionRestController {
 		this.transactionRepository = transactionRepository;
 	}
 
+	//Listaa kaikki myyntitapahtumat
 	@GetMapping // http://localhost:8080/api/transactions
 	ResponseEntity<List<Transaction>> all() {
 		List<Transaction> transactions = transactionRepository.findAll(); // Hae kaikki transaktiot tietokannasta
@@ -30,6 +37,26 @@ public class TransactionRestController {
 		} else {
 			return ResponseEntity.notFound().build();// HTTP 404 Not Found
 		}
+	}
+	
+	//Hae myyntitapahtuma id:llä
+	@GetMapping("/{id}") // http://localhost:8080/api/transactions/1
+	public ResponseEntity<Transaction> getTransaction(@PathVariable Long id) {
+		Optional<Transaction> transaction = transactionRepository.findById(id); // Hae tapahtuma ID:n perusteella
+		if (transaction.isPresent()) {
+			return ResponseEntity.ok(transaction.get()); // HTTP 200 OK
+		} else {
+			return ResponseEntity.notFound().build(); // HTTP 404 Not Found
+		}
+	}
+
+	// Lisää uusi myyntitapahtuma
+	@PostMapping("/") // http://localhost:8080/api/transactions/
+	Transaction newTransaction(@RequestBody Transaction newTransaction) {
+
+		System.out.println("Adding new transaction: " + newTransaction);
+
+		return transactionRepository.save(newTransaction);
 	}
 
 	@DeleteMapping("/{id}") // http://localhost:8080/api/transactions/1
