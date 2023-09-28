c8231c5e3750305e6245676770047582a20d88c4
Nicklas <nicklas.akerman@myy.haaga-helia.fi>
2023-09-28 15:39:43 +0300

    venuerestcontroller tehty

--- src/main/java/Ohjelmistoprojekti/TicketGuru/Event/EventRepository.java
+++ src/main/java/Ohjelmistoprojekti/TicketGuru/Event/EventRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
 	List<Event> findAll();
+	List<Event> findByVenue_VenueId(Long venueId);
 }
