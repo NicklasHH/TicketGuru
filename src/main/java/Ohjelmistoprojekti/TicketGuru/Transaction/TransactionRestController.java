447a86f39d86875bebb0a5a6109ee5ac8de51cf7
Nicklas <nicklas.akerman@myy.haaga-helia.fi>
2023-09-28 13:51:17 +0300

    joku tyhjä viiva varmaan jossain

--- src/main/java/Ohjelmistoprojekti/TicketGuru/Transaction/TransactionRestController.java
+++ src/main/java/Ohjelmistoprojekti/TicketGuru/Transaction/TransactionRestController.java
@@ -13,10 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-
-
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionRestController {
@@ -28,7 +24,7 @@ public class TransactionRestController {
 		this.transactionRepository = transactionRepository;
 	}
 
-	//Listaa kaikki myyntitapahtumat
+	// Listaa kaikki myyntitapahtumat
 	@GetMapping // http://localhost:8080/api/transactions
 	ResponseEntity<List<Transaction>> all() {
 		List<Transaction> transactions = transactionRepository.findAll(); // Hae kaikki transaktiot tietokannasta
@@ -38,8 +34,8 @@ public class TransactionRestController {
 			return ResponseEntity.notFound().build();// HTTP 404 Not Found
 		}
 	}
-	
-	//Hae myyntitapahtuma id:llä
+
+	// Hae myyntitapahtuma id:llä
 	@GetMapping("/{id}") // http://localhost:8080/api/transactions/1
 	public ResponseEntity<Transaction> getTransaction(@PathVariable Long id) {
 		Optional<Transaction> transaction = transactionRepository.findById(id); // Hae tapahtuma ID:n perusteella
@@ -60,8 +56,10 @@ public class TransactionRestController {
 	}
 
 	@DeleteMapping("/{id}") // http://localhost:8080/api/transactions/1
-	public ResponseEntity<?> deleteTransaction(@PathVariable Long id) { // Hae transaktio tietokannasta ja palauta vastaus
-		Optional<Transaction> transactionOptional = transactionRepository.findById(id);// Palauttaa transaktion Id:N perusteella
+	public ResponseEntity<?> deleteTransaction(@PathVariable Long id) { // Hae transaktio tietokannasta ja palauta
+																		// vastaus
+		Optional<Transaction> transactionOptional = transactionRepository.findById(id);// Palauttaa transaktion Id:N
+																						// perusteella
 		if (transactionOptional.isPresent()) {
 			Transaction transaction = transactionOptional.get();
 			transactionRepository.deleteById(id); // Poistaa transaktion Id:n perusteella
