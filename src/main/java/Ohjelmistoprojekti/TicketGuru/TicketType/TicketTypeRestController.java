4e478cbb1e5084c047ed7f3a53ea602c96025074
Vilma Parikainen <bgz832@myy.haaga-helia.fi>
2023-10-08 17:54:44 +0300

    tickettypes validoinnit

--- src/main/java/Ohjelmistoprojekti/TicketGuru/TicketType/TicketTypeRestController.java
+++ src/main/java/Ohjelmistoprojekti/TicketGuru/TicketType/TicketTypeRestController.java
@@ -8,17 +8,22 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import Ohjelmistoprojekti.TicketGuru.Ticket.Ticket;
 import Ohjelmistoprojekti.TicketGuru.Ticket.TicketRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/tickettypes")
@@ -72,7 +77,7 @@ public class TicketTypeRestController {
 	
 	// Luodaan uusi lipputyyppi
 	@PostMapping // http://localhost8080/api/tickettypes
-	public ResponseEntity<TicketType> addTicketType(@RequestBody TicketType ticketType) {
+	public ResponseEntity<TicketType> addTicketType(@Valid @RequestBody TicketType ticketType) {
 		Optional<TicketType> foundTicketType = ticketTypeRepository.findByTicketType(ticketType.getTicketType());
 		
 		if(foundTicketType.isPresent()) {
@@ -85,10 +90,11 @@ public class TicketTypeRestController {
 	
 	// Muokataan lipputyyppiä
 	@PutMapping("/{id}") // http://localhost808/api/tickettypes/1
-	public ResponseEntity<Object> updateTicketType(@RequestBody TicketType editedTicketType, @PathVariable long id) {
+	public ResponseEntity<Object> updateTicketType(@Valid @RequestBody TicketType editedTicketType, @PathVariable long id) {
 		if (!ticketTypeRepository.existsById(id)) {
-			return ResponseEntity.notFound().build(); // HTTP ERROR 404 NOT FOUND
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tickettypea ei löytynyt id:llä " + id); // HTTP ERROR 404 NOT FOUND
 		}
+
 		editedTicketType.setTicketTypeId(id);
 		TicketType updatedTicketType = ticketTypeRepository.save(editedTicketType);
 		return ResponseEntity.ok(updatedTicketType); // HTTP OK 200
@@ -113,6 +119,19 @@ public class TicketTypeRestController {
 		} else {
 			return ResponseEntity.notFound().build(); // HTTP 404 Not Found
 		}
+		
+		
+	}
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationdExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
 	}
 
 }
