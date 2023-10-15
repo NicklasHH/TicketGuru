65bd4bd91d73fc6abac3f1b379dc90fd86d5f34c
Nicklas <nicklas.akerman@myy.haaga-helia.fi>
2023-10-15 16:11:08 +0300

    Muutettu securityConfig muotoon SecurityConfig

--- src/main/java/Ohjelmistoprojekti/TicketGuru/config/SecurityConfig.java
+++ src/main/java/Ohjelmistoprojekti/TicketGuru/config/SecurityConfig.java
@@ -16,43 +16,33 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); 
+	}
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user1 = User.withUsername("user1")
-                .password(passwordEncoder().encode("pass1"))
-                .roles("USER")
-                .build();
-        UserDetails user2 = User.withUsername("user2")
-                .password(passwordEncoder().encode("pass2"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withUsername("admin")
-                .password(passwordEncoder().encode("adminPass"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user1, user2, admin);
-    }
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails user1 = User.withUsername("user1").password(passwordEncoder().encode("pass1")).roles("USER")
+				.build();
+		UserDetails user2 = User.withUsername("user2").password(passwordEncoder().encode("pass2")).roles("USER")
+				.build();
+		UserDetails admin = User.withUsername("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(user1, user2, admin);
+	}
 
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorize -> {
-                    authorize.anyRequest().permitAll(); // Kaikki polut vaativat autentikoinnin
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests(authorize -> {
+			authorize.anyRequest().authenticated(); // Kaikki polut vaativat autentikoinnin
 //                    authorize.requestMatchers("/esimerkki").permitAll(); // Sallii kaikki polut kaikille
 //                    authorize.requestMatchers("/adminEsimerkki").hasRole("ADMIN"); // Vaatii admin roolin
-                }
-                )
+		})
 //                .formLogin(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
-                // Sallii H2-Consoleen pääsyn
-                .headers(headers -> headers.frameOptions().sameOrigin());
-        return http.build();
-    }
+				.csrf(AbstractHttpConfigurer::disable).httpBasic(Customizer.withDefaults())
+				// Sallii H2-Consoleen pääsyn
+				.headers(headers -> headers.frameOptions().sameOrigin());
+		return http.build();
+	}
 }
