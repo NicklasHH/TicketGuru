7d8106713ebe4e8469fd97371e63014d58c9ebd1
laguagu <49702497+laguagu@users.noreply.github.com>
2023-11-14 17:22:46 +0200

    testGetTicketByIdWithOutAuth and testFindKorvatunturi

--- src/test/java/Ohjelmistoprojekti/TicketGuru/IntegrationTest.java
+++ src/test/java/Ohjelmistoprojekti/TicketGuru/IntegrationTest.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -32,9 +30,22 @@ public class IntegrationTest {
     }
 
     @Test
+    public void testGetTicketByIdWithOutAuth() throws Exception {
+        mockMvc.perform(get("/api/tickets/1"))
+                .andExpect(status().isUnauthorized());
+    }
+    @Test
     public void testHomeScreenWelcome() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Welcome to TicketGurdu")));
+                .andExpect(content().string(containsString("Welcome to TicketGuru")));
     }
+
+    @Test
+    public void testFindKorvatunturi() throws Exception {
+        mockMvc.perform(get("/api/postalcodes"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[?(@.postalcode == '99999')].postOffice").value("Korvatunturi"));
+    }
+
 }
